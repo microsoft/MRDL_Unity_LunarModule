2bca3e3eabfbc83f5bb0a4189a04b81926808345
johnppella <johnppella@live.com>
2018-04-02 10:51:46 -0700

    moving progress from MRTK into MRDL

--- Assets/MRDesignLab_LunarModule/Scripts/ThrottleDisplay.cs
+++ Assets/MRDesignLab_LunarModule/Scripts/ThrottleDisplay.cs
@@ -2,8 +2,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 //
-using HUX;
-using HUX.Utility;
 using UnityEngine;
 
 namespace MRDL
